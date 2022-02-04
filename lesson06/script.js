a2fe2b976d478b03d6dc87c931bb16e401da3639
David Melnik <davidmelnik483@gmail.com>
2022-02-04 14:18:43 +0200

    добавил обязательное

--- lesson06/script.js
+++ lesson06/script.js
@@ -8,46 +8,80 @@ const randomNum = function (n) {
   return Math.floor(Math.random() * Math.floor(n));
 };
 
+// Обязательное
+
 const runGame = function () {
   let rightAnswer = randomNum(100);
-  let attempt = 10;
+  console.log(rightAnswer);
   const game = function () {
-    attempt--;
-    if (attempt < 0) {
-      if (confirm("Попытки закончились, хотите сыграть еще?")) {
-        runGame();
+
+    const input = prompt("Угадай число от 1 до 100");
+    if (input === null) {
+      alert("Игра окончена!");
+      return;
+    }
+    if (isNumber(input)) {
+      const yourNumber = +input;
+      if (yourNumber > rightAnswer) {
+        alert("Загаданное число больше");
+        game();
+      } else if (yourNumber < rightAnswer) {
+        alert("Загаданное число меньше");
+        game();
       } else {
-        alert("До свидания!");
-        return;
+        alert("Поздравляю, Вы угадали!!!")
       }
     } else {
-      const input = prompt("Угадай число от 1 до 100");
-      if (input === null) {
-        alert("Игра окончена!");
-        return;
-      }
-      if (isNumber(input)) {
-        const yourNumber = +input;
-        if (yourNumber > rightAnswer) {
-          alert(`Загаданное число больше, осталось ${attempt} попыток.`);
-          game();
-        } else if (yourNumber < rightAnswer) {
-          alert(`Загаданное число меньше, осталось ${attempt} попыток.`);
-          game();
-        } else {
-          if (confirm("Поздравляю, вы угадали! Сыграем еще?")) {
-            runGame();
-          } else {
-            alert("Игра окончена!");
-            return;
-          }
-        }
-      } else {
-        alert("Введите число");
-        game();
-      }
+      alert("Введите число");
+      game();
     }
   };
   game();
 };
 runGame();
+
+// Усложненное
+
+// const runGame = function () {
+//   let rightAnswer = randomNum(100);
+//   let attempt = 10;
+//   const game = function () {
+//     attempt--;
+//     if (attempt < 0) {
+//       if (confirm("Попытки закончились, хотите сыграть еще?")) {
+//         runGame();
+//       } else {
+//         alert("До свидания!");
+//         return;
+//       }
+//     } else {
+//       const input = prompt("Угадай число от 1 до 100");
+//       if (input === null) {
+//         alert("Игра окончена!");
+//         return;
+//       }
+//       if (isNumber(input)) {
+//         const yourNumber = +input;
+//         if (yourNumber > rightAnswer) {
+//           alert(`Загаданное число больше, осталось ${attempt} попыток.`);
+//           game();
+//         } else if (yourNumber < rightAnswer) {
+//           alert(`Загаданное число меньше, осталось ${attempt} попыток.`);
+//           game();
+//         } else {
+//           if (confirm("Поздравляю, вы угадали! Сыграем еще?")) {
+//             runGame();
+//           } else {
+//             alert("Игра окончена!");
+//             return;
+//           }
+//         }
+//       } else {
+//         alert("Введите число");
+//         game();
+//       }
+//     }
+//   };
+//   game();
+// };
+// runGame();
